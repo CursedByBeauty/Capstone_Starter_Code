7251d8d346fdd2ae702cdc8dcacc018de5bfcd19
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-05 11:48:38 -0400

    Modded views in worker_response.

--- backend/worker_response/views.py
+++ backend/worker_response/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from .models import Workorders
+from .serializers import WorkerResponseSerializer
+from modulefinder import IMPORT_NAME
+from rest_framework import status
+from rest_framework.response import Response 
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def workorders_list(request):
+    
+    if request.method == 'GET':
+        ticket = Workorders.objects.all()
+        serializer = WorkerResponseSerializer(ticket, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = WorkerResponseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def workorders_detail(request, pk):
+    task = get_object_or_404(Workorders, pk=pk)
+    if request.method == 'GET':
+        serializer = WorkerResponseSerializer(task)
+        return Response(serializer.data)
+    elif request.method == 'PUT': 
+        serializer = WorkerResponseSerializer(Workorders, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
