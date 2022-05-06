48f5c106de9fbdc626be1a144a06996e07b22d5a
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-06 12:08:52 -0400

    Modded App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 // General Imports
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
+import useAuth from "./hooks/useAuth";
 
 // Pages Imports
-import HomePage from "./pages/HomePage/HomePage";
+import WorkordersPage from "./pages/WorkordersPage/WorkordersPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
@@ -15,18 +18,36 @@ import Footer from "./components/Footer/Footer";
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
+  const [tickets, setTickets] = useState([]);
+  const [user, token] = useAuth();
+
+  useEffect(() => {
+    getAllTickets();
+  }, []);
+
+  async function getAllTickets() {
+    try {
+      let response = await axios.get("http://127.0.0.1:8000/api/workorders/", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div>
       <Navbar />
-      <Routes>
+      <PrivateRoute>
         <Route
           path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
+          element={<WorkordersPage tickets={tickets} setTickets={setTickets} />}
         />
+      </PrivateRoute>
+      <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
---
+++ frontend/src/pages/WorkordersPage/WorkordersPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const WorkordersPage = (props) => {
+    return ( 
+        <div>
+            <h1>Testing</h1>
+        </div>
+     );
+}
+ 
+export default WorkordersPage;
