ae07f710ea61304a97a2e8486c5f26c22b96372e
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-09 21:02:08 -0400

    Debugged the WorkorderPage axios call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,76 @@
 // General Imports
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import axios from "axios";
+import useAuth from "./hooks/useAuth";
 
 // Pages Imports
-import WorkordersPage from './pages/WorkordersPage/WorkordersPage'
+import WorkordersPage from "./pages/WorkordersPage/WorkordersPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import PortalChoicePage from './pages/PortalChoicePage/PortalChoicePage';
-import ResidentPage from './pages/ResidentPage/ResidentPage';
+import PortalChoicePage from "./pages/PortalChoicePage/PortalChoicePage";
+import ResidentPage from "./pages/ResidentPage/ResidentPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import ResponsePage from './pages/ResponsePage/ResponsePage';
+import ResponsePage from "./pages/ResponsePage/ResponsePage";
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-const [tickets,setTickets] = useState([])
+  const [user, token] = useAuth();
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(
+    (props) => {
+      const getAllTickets = async () => {
+        try {
+          let response = await axios.get(
+            "http://127.0.0.1:8000/api/workorders/",
+            {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          setTickets(response.data);
+        } catch (error) {
+          console.log(error.message);
+          console.log(tickets);
+        }
+      };
+      getAllTickets();
+    },
+    [token]
+  );
+
   return (
     <div className="page">
       <Navbar />
       <Routes>
-        <Route path="/" element={<PortalChoicePage/>}/>
-        <Route path="/maintenance" element={<PrivateRoute><WorkordersPage setTickets = {setTickets} tickets = {tickets}/></PrivateRoute>}/>
-        <Route path="/resident" element={<PrivateRoute><ResidentPage/></PrivateRoute>}/>
-        <Route path="/response/:ticketId/" element={<ResponsePage setTickets = {setTickets} tickets = {tickets} />}/>
+        <Route path="/" element={<PortalChoicePage />} />
+        <Route
+          path="/maintenance"
+          element={
+            <PrivateRoute>
+              <WorkordersPage setTickets={setTickets} tickets={tickets} />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/resident"
+          element={
+            <PrivateRoute>
+              <ResidentPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/response/:ticketId/"
+          element={<ResponsePage setTickets={setTickets} tickets={tickets} />}
+        />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
@@ -35,6 +78,5 @@ const [tickets,setTickets] = useState([])
     </div>
   );
 }
-
 
 export default App;
--- frontend/src/pages/WorkordersPage/WorkordersPage.js
+++ frontend/src/pages/WorkordersPage/WorkordersPage.js
@@ -1,35 +1,13 @@
 import React, { useEffect } from 'react';
-import axios from "axios";
-import useAuth from '../../hooks/useAuth';
+
 import DisplayWorkorders from '../../components/DisplayWorkorders/DisplayWorkorders';
 
 
 const WorkordersPage = (props) => {
-    const [user,token] = useAuth()
-    
-  
-    useEffect((props) => {
-      const getAllTickets = async () => {
-        try {
-          let response = await axios.get("http://127.0.0.1:8000/api/workorders/", {
-            headers: {
-              Authorization: "Bearer " + token,
-            },
-          });
-          props.setTickets(response.data);
-        } catch (error) {
-          console.log(props.tickets)
-          console.log(error.message);
-        }
-      };
-      getAllTickets();
-    }, [token]);
-  
     return ( 
         <div>
             <DisplayWorkorders tickets={props.tickets}/>
         </div>
      );
 }
- 
 export default WorkordersPage;
